ce5efccefd0e497f7d71e33607f59d29ee09ccff
Axel Duvacher <duvacher.axel@protonmail.com>
2023-11-27 21:41:33 +0100

    feat: solution axel s'echauffe

--- apps/angular/crud/src/app/app.component.ts
+++ apps/angular/crud/src/app/app.component.ts
@@ -3,28 +3,36 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Todo } from './model/todo';
 import { ToDoListComponent } from './UI/toDoList.component';
 import { ToDoListService } from './service/toDoList.service';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Component({
   standalone: true,
   imports: [CommonModule, ToDoListComponent],
   selector: 'app-root',
   template: `
-    <app-toDoList [todos]="todos" (todoUpdate)="update($event)"></app-toDoList>
+    <app-toDoList
+      [todos]="(todos$ | async) || []"
+      (todoUpdate)="update($event)"></app-toDoList>
   `,
   styles: [],
 })
 export class AppComponent implements OnInit {
   toDoListService = inject(ToDoListService);
-  todos: Todo[] = [];
+  todos$ = new BehaviorSubject<Todo[]>([]);
 
   ngOnInit(): void {
-    this.toDoListService.getToDo().subscribe((todo) => (this.todos = todo));
+    this.toDoListService
+      .getToDo()
+      .pipe(tap((todos) => this.todos$.next(todos)))
+      .subscribe();
   }
 
   update(todo: Todo) {
     this.toDoListService.saveToDo(todo).subscribe((todoUpdated) => {
-      console.log('yo');
-      this.todos[todoUpdated.id - 1] = todoUpdated;
+      const todos = this.todos$.value;
+      todos[todoUpdated.id - 1] = todoUpdated;
+
+      this.todos$.next(todos);
     });
   }
 }
